c4616f41b0c73c85d4fea2c33cad297e1db456c3
brainmurphy1 <brianmurphy@gatech.edu>
2015-03-21 15:59:50 -0400

    switched to java...

---
+++ practice.py
@@ -0,0 +1,11 @@
+from numpy  import *
+
+a = array( [2,3,4] )
+b = array( [ (1.5,2,3), (4,5,6) ] )
+c = array( [ [1,2], [3,4] ], dtype=complex )
+d = arange(6)   
+
+
+
+
+print d;
---
+++ src/HilbertOps.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+/**
+ * Created by Brian on 3/21/2015.
+ */
+public class HilbertOps {
+
+    public static double[][] generateHilbertMatrix(int width){
+
+        double[][] H = new double[width][width];
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < width; j++) {
+                H[i][j] = 1.0 / (i + j + 1);
+            }
+        }
+
+        return H;
+    }
+
+    public static void main(String[] args) {
+        printMatrix(generateHilbertMatrix(5));
+    }
+
+    public static void printMatrix(double[][] matrix) {
+        for (double[] v : matrix) {
+            System.out.println(Arrays.toString(v));
+        }
+    }
+}
