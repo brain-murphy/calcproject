b671e72bdecd8da1d668b2611f56050baa6c3258
jjoe8 <randomspersons@gmail.com>
2015-03-21 17:18:02 -0400

    Create PowerMethod.java

---
+++ src/PopDynamics/PowerMethod.java
@@ -0,0 +1,21 @@
+public class PowerMethod {
+    double[][] a;
+    double tol;
+    double[][] initial;
+    double eigenValue;
+    double eigenVector[][];
+
+    public int PowerMethod(double[][] a, double tol, double[][] initial) {
+        this.a = a;
+        this.tol = tol;
+        this.initial = initial;
+    }
+
+    public int findErrorTol() {
+
+    }
+
+    public int powerMethod(double[][] a, double[][] initial) {
+        
+    } 
+}
